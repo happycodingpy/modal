aa8f63b7c02d6bb2770e8f86ea52a56222da64e0
happycodingpy <hugo.orlando2030@gmail.com>
2021-02-17 10:42:18 -0300

    js

--- js/script.js
+++ js/script.js
@@ -5,17 +5,29 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-
-  const closeModel = function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  };
-
-  btnCloseModal.addEventListener('click', closeModel);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if ((e.key === 'Escape' && !modal.classList.contains('hidden')){
+      closeModal()
+  }
+
+ 
+});
